022fe2a7656fe90a462b2397d97d60a0a8abc4f6
muzzii255 <muzammil255@gmail.com>
2025-07-13 22:18:34 -0400

    bug fix

--- main.py
+++ main.py
@@ -48,6 +48,24 @@ app.add_middleware(
 )
 
 
+
+def verify_api_key(request: Request):
+    auth_header = request.headers.get("authorization")
+
+    if not auth_header:
+        raise HTTPException(status_code=401, detail="API key required")
+
+    if auth_header.startswith("Bearer "):
+        api_key = auth_header[7:]
+    else:
+        api_key = auth_header
+
+    if api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API key")
+
+    return True
+
+
 def get_task_info(task_id: str) -> Dict[str, Any]:
     try:
         result = AsyncResult(task_id, app=celery_app)
@@ -67,7 +85,7 @@ def get_task_info(task_id: str) -> Dict[str, Any]:
 
 
 @app.get("/api/v1/task/{task_id}", response_model=TaskStatusResponse)
-async def get_task_status(task_id: str):
+async def get_task_status(task_id: str, _: bool = Depends(verify_api_key)):
     try:
         task_info = get_task_info(task_id)
         return TaskStatusResponse(**task_info)
@@ -77,21 +95,8 @@ async def get_task_status(task_id: str):
 
 
 @app.post("/api/v1/scrape/username", response_model=TaskResponse)
-async def scrape_tariff(request: UserRequest):
+async def scrape_username(request: UserRequest, _: bool = Depends(verify_api_key)):
     try:
-        auth_header = request.headers.get("authorization")
-        
-        if not auth_header:
-            raise HTTPException(status_code=401, detail="API key required")
-        
-        if auth_header.startswith("Bearer "):
-            api_key = auth_header[7:]
-        else:
-            api_key = auth_header
-        
-        if api_key != API_KEY:
-            raise HTTPException(status_code=401, detail="Invalid API key")
-        
         task = scrape_insta.delay(
             request.username
         )
@@ -107,12 +112,10 @@ async def scrape_tariff(request: UserRequest):
 
 
 @app.post("/api/v1/get-user")
-async def get_user(request:UserRequest):
+async def get_user(request:UserRequest, _: bool = Depends(verify_api_key) ):
     try:
         data = get_user_with_posts(request.username)
         return data
     except Exception as e:
         print(e)
         raise HTTPException(status_code=500, detail=str(e))
-        
-        
